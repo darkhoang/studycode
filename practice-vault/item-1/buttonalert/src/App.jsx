721f23513e10b0db3d664a9ea9436050c5428e66
HoangTruong <tmhoangc12th@gmail.com>
2024-08-28 16:51:05 +0700

    review homework

---
+++ practice-vault/item-1/buttonalert/src/App.jsx
@@ -0,0 +1,15 @@
+import react from 'react';
+import './App.css';
+
+function App() {
+  const onClick = () => {
+    alert('Button clicked');
+  };
+  return (
+    <div className="container">
+      <button onClick={onClick}>Click me to open an alert</button>
+    </div>
+  );
+}
+
+export default App;
--- practice-vault/item-2/src/App.jsx
+++ practice-vault/item-2/src/App.jsx
@@ -2,19 +2,15 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
-  const onABCClick = () => {
-    setInputValue('A button is clicked');
+  const [inputValue, setisinputValue] = useState('');
+  const buttonClick = () => {
+    setisinputValue('A button is clicked');
   };
-
   return (
     <div className="container">
-      <input type="text" className="input" value={inputValue} />
-      <button className="button" onClick={onABCClick}>
-        Click me to change the input value
-      </button>
+      <input placeholder="Lets click on Button" value={inputValue} />
+      <button onClick={buttonClick}>Click me to change the input value</button>
     </div>
   );
 }
-
 export default App;
