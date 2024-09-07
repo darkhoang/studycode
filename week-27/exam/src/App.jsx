06ed9ab3451b5e359d98150e1088358bcd03931f
Hoang Truong <your-github-email@example.com>
2024-09-07 11:14:48 +0700

    study array.map again

--- week-27/exam/src/App.jsx
+++ week-27/exam/src/App.jsx
@@ -1,50 +1,57 @@
 import { useState } from 'react';
 import './App.css';
 
+const colors = ['red', 'green', 'blue', 'purple', 'orange', 'black'];
+
+// array.map - chuyen hoa 1 array to a different array
+
 function App() {
   // Khởi tạo trạng thái màu nền, mặc định là 'white'
   const [backgroundColor, setBackgroundColor] = useState('white');
 
-  // Hàm để cập nhật màu nền khi nhấn nút
-  const onClickRed = () => {
-    setBackgroundColor('red'); // Cập nhật màu nền thành đỏ
+  console.log(backgroundColor);
+
+  const onclickButton = (color) => {
+    setBackgroundColor(color);
   };
+  // tao 1 cai array chua buttons ma co ten la tu colors o tren
 
   return (
-    <div className="container" style={{ backgroundColor }}>
-      <button className="color-red" onClick={onClickRed}>
-        Red
-      </button>
-
-      <button
-        className="color-green"
-        onClick={() => setBackgroundColor('green')}
-      >
-        Green
-      </button>
-      <button className="color-blue" onClick={() => setBackgroundColor('blue')}>
-        Blue
-      </button>
-      <button
-        className="color-purple"
-        onClick={() => setBackgroundColor('purple')}
-      >
-        Purple
-      </button>
-      <button
-        className="color-orange"
-        onClick={() => setBackgroundColor('orange')}
-      >
-        Orange
-      </button>
-      <button
-        className="color-black"
-        onClick={() => setBackgroundColor('black')}
-      >
-        Black
-      </button>
+    <div className={`container color-${backgroundColor}`}>
+      <div className="content">
+        {colors.map((color) => (
+          <button
+            className={`color-${color}`}
+            onClick={() => {
+              onclickButton(color);
+            }}
+          >
+            {color}
+          </button>
+        ))}
+      </div>
     </div>
   );
+  // return (
+  //   <div className={`container color-${backgroundColor}`}>
+  //     {colors.map((color) => {
+  //       // const onclickButton = () => {
+  //       //   setBackgroundColor(color);
+  //       // };
+
+  //       return (
+  //         <button
+  //           className={`color-${color}`}
+  //           onClick={() => {
+  //             onclickButton(color);
+  //           }}
+  //         >
+  //           {color}
+  //         </button>
+  //       );
+  //     })}
+  //   </div>
+  // );
 }
 
 export default App;
